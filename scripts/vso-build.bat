21cdd345aaeee790871ba9dd3fb79846f8053d81
Karl Burtram <karlb@microsoft.com>
2018-01-30 12:52:44 -0800

    Update build script

--- scripts/vso-build.bat
+++ scripts/vso-build.bat
@@ -3,7 +3,9 @@ git rm --cached -r .
 # install chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install yarn -y
+choco install yarn -y -f
+
+WHERE yarn
 
 SET "PATH=%PATH%;%ProgramFiles(x86)%\yarn;%ProgramFiles%\yarn"
 
