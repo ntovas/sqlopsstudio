3f80ae2f1599f2a837720a39dd7ca496ec880919
Karl Burtram <karlb@microsoft.com>
2018-01-30 14:24:53 -0800

    Use npm 1

--- scripts/vso-build.bat
+++ scripts/vso-build.bat
@@ -1,42 +1,10 @@
 git config core.autocrlf false
 git rm --cached -r .
-# install chocolatey
-@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-DEL C:\ProgramData\chocolatey\logs\chocolatey.log /f
+npm install --global yarn
+npm install yarn
 
-choco uninstall yarn -y -f -v
-
-choco install yarn -y -f -v
-
-type C:\ProgramData\chocolatey\logs\chocolatey.log
-
-WHERE yarn
-
-ECHO %CD%
-
-
-SET "PATH=%PATH%;%ProgramFiles(x86)%\yarn;%ProgramFiles%\yarn"
-
-ECHO %AGENT_HOMEDIRECTORY%
-ECHO %AGENT_BUILDDIRECTORY%
-ECHO %AGENT_WORKFOLDER%
-
-DIR  %AGENT_WORKFOLDER%\_temp
-DIR  %AGENT_WORKFOLDER%\_temp\chocolatey
-DIR  %AGENT_WORKFOLDER%\_temp\chocolatey\yarn
-DIR  %AGENT_WORKFOLDER%\_temp\chocolatey\yarn\1.3.2
-TYPE  %AGENT_WORKFOLDER%\_temp\chocolatey\yarn\yarn.MsiInstall.log
-
-DIR C:\ProgramData
-
-SET
-
-DIR d:\
-
-
-
-scripts/npm.bat install --arch=x64
+scripts\npm.bat install --arch=x64
 
 node .\node_modules\gulp\bin\gulp.js compile
 
